6bb704083c125a328599b4eb82879d20b8d6c158
Ananthu P <ananthup303@gmail.com>
2026-03-17 19:46:01 +0530

    Update new

--- chat/apps.py
+++ chat/apps.py
@@ -2,9 +2,3 @@ from django.apps import AppConfig
 class ChatConfig(AppConfig):
     name = "chat"
 
-    def ready(self):
-        from .models import User
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser(
-                username="admin", email="admin@gmail.com", password="Testpassword@123"
-            )
